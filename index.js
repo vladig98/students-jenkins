32b445603556dd6555982ff468cd1c012d66e0c1
Vladimir Gotsin <32268856+vladig98@users.noreply.github.com>
2024-06-15 17:35:34 +0300

    Changing port

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 app.set('view engine', 'pug');
 app.use(require('body-parser')
-  .urlencoded({extended:true}));
+  .urlencoded({ extended: true }));
 
-const studentsController = 
+const studentsController =
   require("./controllers/students-controller");
 
 let students = require("./models/students-model");
@@ -13,14 +13,14 @@ studentsController.setup(app, students);
 
 let port = process.argv[2];
 if (!port) port = process.env['PORT'];
-if (!port) port = 8080;
+if (!port) port = 3030;
 
 app.listen(port, () => {
   console.log(`App started. Listening at http://localhost:${port}`);
 })
-.on('error', function(err) {
-  if (err.errno === 'EADDRINUSE')
-    console.error(`Port ${port} busy.`);
-  else 
-    throw err;
-});
+  .on('error', function (err) {
+    if (err.errno === 'EADDRINUSE')
+      console.error(`Port ${port} busy.`);
+    else
+      throw err;
+  });
